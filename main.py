afc1558535015db65a32e17a3af6d0c112e5b7e2
poulpette <apertureproject.me@gmail.com>
2020-03-01 18:48:13 +0100

    Add code

--- main.py
+++ main.py
@@ -1,1 +1,58 @@
+import asyncio
+import json
+import logging
+import os
+import sys
+import discord
+from discord.ext import commands
 
+print('All modules successfully imported.')
+
+with open('./config.json', 'r') as cjson:
+    config = json.load(cjson)
+
+logging.basicConfig(level='INFO')
+
+PREFIX = config["prefix"]
+MODULES = config["modules"]
+OWNER = config["owner_id"]
+VERSION = config["version"]
+TOKEN = os.environ["TOKEN"]
+
+bot = commands.Bot(command_prefix=[PREFIX])
+
+bot.config = config
+bot.ready = False
+
+
+async def status():
+    while True:
+        names = [f'\'nya help\' !', 'on my supportserv, nya.', f'on {len(bot.guilds)} servers, nya !',
+                 f'with {len(bot.users)} users. Nya.']
+        for name in names:
+            await bot.change_presence(activity=discord.Game(name=name))
+            await asyncio.sleep(120)
+
+
+@bot.event
+async def on_connect():
+    print('Successfully connected !')
+
+
+@bot.event
+async def on_ready():
+    loaded = len(MODULES)
+    bot.remove_command('help')
+    for module in modules:
+        try:
+            logging.info('Loading %s', module)
+            bot.load_extension(f'modules.{module}')
+        except Exception as e:
+            logging.exception('Failed to load {} : {}'.format(module, e))
+    print('Logged in.')
+    print('{}/{} modules loaded'.format(loaded, len(MODULES)))
+    print(f'discord.py lib version : {discord.__version__}')
+    bot.loop.create_task(status())
+
+
+bot.run(TOKEN)
