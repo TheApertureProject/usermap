4bf969a5a8ba75502f05788f4cd0519ba97608bb
poulpette <apertureproject.me@gmail.com>
2020-03-01 20:28:58 +0100

    Update help.py

--- modules/help.py
+++ modules/help.py
@@ -21,7 +21,9 @@ class Help(commands.Cog):
 
     @commands.command(aliases=["information", "about"])
     async def info(self, ctx):
-        e = discord.Embed(title="$username", description="Un robot à but sécuritaire et informatif")
+        e = discord.Embed(title="$username", description="Un robot d'analyse pour votre serveur.")
+        e.add_field(name='✨ Liens utiles', value='[Lien d\'invite](http://bit.ly/2uIQwmh) | [Serveur de support](https://discord.gg/H7ZcCB) | [Site web du développeur](https://apertureproject.me/)')
+        e.set_image(url='https://cdn.discordapp.com/attachments/683756494369587200/683756519967293467/usermap_banner-01.png')
         await ctx.send(embed=e)
 
 def setup(bot):
