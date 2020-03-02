64ed560424c70144f8447268e386802c07a16c5b
poulpette <apertureproject.me@gmail.com>
2020-03-02 13:36:02 +0100

    Update datacom.py

--- modules/datacom.py
+++ modules/datacom.py
@@ -24,19 +24,20 @@ class Datacom(commands.Cog):
     async def search(self, ctx, playerid:int):
         user1 = db.reported_user.find_one({'u_id' : playerid})
         if user1 == "None":
-            await ctx.send(embed = discord.Embed(title="Résultat de la recherche", description="❌ Aucune entrée associée à cet ID n'a pu être identifiée.", color=0xffff00)
+            a = discord.Embed(title="Résultat de la recherche", description="❌ Aucune entrée associée à cet ID n'a pu être identifiée.", color=0xffff00)
+            await ctx.send(embed=a)
         else:
-            UNAME=user1["u_name"]
-            UID=user1["u_id"]
-            UARTICLES=user1["u_articles"]
-            UINFRINGMENT=user1["u_infringment"]
-            ULEVEL=user1["u_level"]
+            UNAME = user1["u_name"]
+            UID = user1["u_id"]
+            UARTICLES = user1["u_articles"]
+            UINFRINGMENT = user1["u_infringment"]
+            ULEVEL = user1["u_level"]
 
             e = discord.Embed(title="Résultat de la recherche", description="✨ Une entrée a été trouvée. Consultez la suite du rapport pour plus de détails)
 
             e.add_field(name=f"Rapport de l'utilisateur `{UNAME}`", value=f"""Niveau : `{ULEVEL}`
             Articles enfreints (`{len(UARTICLES)}`) : `{UARTICLES}`
-            Détails : {u_infringment}""")
+            Détails : {UINFRINGMENT}""")
 
             if ULEVEL == 1:
                 bannerurl="http://bit.ly/38gY1i2"
@@ -44,6 +45,7 @@ class Datacom(commands.Cog):
                 bannerurl="http://bit.ly/38hmweM"
             else:
                 bannerurl="http://bit.ly/2IcJzNq"
+            
             e.set_image(url=bannerurl)
 
             await ctx.send(embed=e)
